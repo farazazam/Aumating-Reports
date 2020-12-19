aa716acc0c3c27064caa81dc05b8157605b40c87
farazazam <73869701+farazazam@users.noreply.github.com>
2020-12-19 04:46:35 -0600

    Add files via upload

---
+++ word cloud.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', '')
+from pathlib import Path
+from textblob import TextBlob
+blob = TextBlob(Path('RomeoAndJuliet.txt'). read_text())
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+items = blob.word_counts.items()
+items = [item for item in items if item[0] not in stop_words]
+from operator import itemgetter
+sorted_items= sorted(items,key=itemgetter(1),reverse=True)
+top20 = sorted_items[0:20]
+
+import pandas as pd
+df = pd.DataFrame(top20, columns=['word','count'])
+axes = df.plot.bar(x= 'word', y='count',legend=False)
+
+
+from pathlib import Path
+text = Path('RomeoAndJuliet.txt').read_text()
+import imageio
+mask_image = imageio.imread('mask_heart.png')
+from wordcloud import WordCloud
+wordcloud = WordCloud(width=1000, height=1000, colormap='prism', mask=mask_image, background_color='white')
+wordcloud = wordcloud.generate(text)
+
